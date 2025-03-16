cddfeece9b50e9751d831bc070b75758c8848300
hoodmanb <joshuadebravo@gmail.com>
2025-03-16 16:38:15 +0100

    configured axios for production and developent

--- shopping/app/hooks/axiosClient.js
+++ shopping/app/hooks/axiosClient.js
@@ -4,21 +4,29 @@ import axios from 'axios';
  * Reusable Axios Client
  * @param {string} url - The request URL
  * @param {string} method - HTTP method (GET, POST, PUT, DELETE, etc.)
- * @param {object} [data={}] - Request payload (for POST, PUT)
+ * @param {object} [data={}] - Request payload (for POST, PUT, PATCH)
  * @param {string} [token=null] - Optional authorization token
  * @returns {Promise<object>} - Response data or error
  */
 const axiosClient = async (url, method = 'GET', data = {}, token = null) => {
   try {
+    const environment = process.env.NODE_ENV;
+    let baseURL = '';
+
+    if (environment === 'production') {
+      baseURL = 'https://shopping-app-0ig3.onrender.com';
+    }
+
     const headers = {
       'Content-Type': 'application/json',
       ...(token && { Authorization: `Bearer ${token}` }),
     };
 
     const response = await axios({
-      url,
+      baseURL, // Use baseURL instead of baseUrl
+      url, // Separate url key instead of combining it with baseURL
       method,
-      data: ['POST', 'PUT', 'PATCH', 'GET', 'DELETE'].includes(method.toUpperCase()) ? data : null,
+      data: ['POST', 'PUT', 'PATCH'].includes(method.toUpperCase()) ? data : undefined, // Only include data when necessary
       headers,
     });
 
